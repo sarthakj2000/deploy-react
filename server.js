566e727cb84b13c96a53d355f85635cdcf549358
Sarthak Jaiswal <sarthakjaiswal45@gmail.com>
2024-08-26 21:45:24 +0530

    commit

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-const express=require('express');
-const connectDB=require('./config/db');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const app=express();
-//connect database
-connectDB();
-const PORT=process.env.PORT || 5000;
-//Init Middleware
-app.use(express.json({extented:true}));//for req.body
-
-
-// app.get('/',(req,res)=>res.json({msg:'welcome to the contact keeper api'}));
-//Define Routes
-app.use('/api/users',require('./routes/users'));
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/contacts',require('./routes/contacts'));
-
-if(process.env.NODE_ENV=="production"){
-    const path=require("path")
-    app.use(express.static('client/build'));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-app.listen(PORT,()=>console.log(`Server started on ${PORT}`));
+// Define API routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
