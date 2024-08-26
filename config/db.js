aebb74bc491f252bf2785c492426da34f508a626
Sarthak Jaiswal <sarthakjaiswal45@gmail.com>
2024-08-26 21:41:16 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -3,20 +3,15 @@ const config=require('config');
 
 const {mongoURI} =require("../config/keys")
 
-const connectDB=async ()=>{
+const connectDB= ()=>{
 
-    try{
-       await mongoose.connect(mongoURI,{
-            useNewUrlParser:true,
-            useCreateIndex:true,
-            useFindAndModify:false,
-            useUnifiedTopology: true
-        });
-        console.log('mongo db connected');
-    }
-    catch(err){
-        console.log(err.message);
-        process.exit(1);
-    }
+       
+
+        mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+    
+
+   
 }
 module.exports=connectDB;
